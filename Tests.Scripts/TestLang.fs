61998f6f3b34ee553988d4b10b496d7742f001a6
jason-kerney <jason@teamkerney.com>
2023-04-01 10:25:31 -0700

    refactor: removed unused method

--- Tests.Scripts/TestLang.fs
+++ Tests.Scripts/TestLang.fs
@@ -4,6 +4,3 @@ module Archer.Tests.Scripts.TestLang.Lang
 open Archer.Tests.Scripts.TestLang.Types
 
 let suite = TestContainerBuilder ()
-
-let newTest fn (container: TestBuilder) =
-    fn container
