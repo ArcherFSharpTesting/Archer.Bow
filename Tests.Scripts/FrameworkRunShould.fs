527d36f2aefeb84c0cc96234e1bca2e594e256b9
jason-kerney <jason@teamkerney.com>
2023-04-01 10:12:57 -0700

    refactor: extracted verify method

--- Tests.Scripts/FrameworkRunShould.fs
+++ Tests.Scripts/FrameworkRunShould.fs
@@ -9,6 +9,13 @@ let getDefaultSeed () = defaultSeed
 
 let private container = suite.Container ("", "Framework Run Should")
 
+let verifyWith expected result =
+    if expected = result then TestSuccess
+    else
+        $"expected \"%A{result}\" to be \"%A{expected}\""
+        |> VerificationFailure
+        |> TestFailure
+
 let ``return empty results when it has no tests`` =
     container
     |> newTest (fun container ->
@@ -24,11 +31,7 @@ let ``return empty results when it has no tests`` =
                     Seed = seed
                 }
                 
-                if expected = result then TestSuccess
-                else
-                    $"expected \"%A{result}\" to be \"%A{expected}\""
-                    |> VerificationFailure
-                    |> TestFailure
+                result |> verifyWith expected
             )
         )
     
@@ -47,34 +50,26 @@ let ``return empty results with specific seed when it has no tests`` =
                     Seed = seed
                 }
                 
-                if expected = result then TestSuccess
-                else
-                    $"expected \"%A{result}\" to be \"%A{expected}\""
-                    |> VerificationFailure
-                    |> TestFailure
+                result |> verifyWith expected
             )
         )
 
 let ``return a successful result when one test passes`` =
     container
     |> newTest (fun container -> container.Test ("return a successful result when one test passes", fun () ->
-        let framework = archer.Framework ()
-        let container = suite.Container ("A Test Suite", "with passing tests")
-        let test = container.Test ("First Passing Test", fun () -> TestSuccess)
+            let framework = archer.Framework ()
+            let container = suite.Container ("A Test Suite", "with passing tests")
+            let test = container.Test ("First Passing Test", fun () -> TestSuccess)
 
-        framework.AddTest test
-        let result = framework.Run (getDefaultSeed)
+            framework.AddTest test
+            let result = framework.Run (getDefaultSeed)
         
-        let expected = {
-            Failures = []
-            Successes = [test]
-            Seed = defaultSeed
-        }
+            let expected = {
+                Failures = []
+                Successes = [test]
+                Seed = defaultSeed
+            }
 
-        if expected = result then TestSuccess
-        else
-            $"expected \"%A{result}\" to be \"%A{expected}\""
-            |> VerificationFailure
-            |> TestFailure
+            result |> verifyWith expected
         )
     )
