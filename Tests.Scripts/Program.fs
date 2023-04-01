cc4f89a532ec6033e5e1528e7f99d3eb2d640a4e
jason-kerney <jason@teamkerney.com>
2023-04-01 10:50:19 -0700

    chore: Runing tests with test framework

--- Tests.Scripts/Program.fs
+++ Tests.Scripts/Program.fs
@@ -2,26 +2,29 @@
 // For more information see https://aka.ms/fsharp-console-apps
 
 open Archer.CoreTypes.Lib
+open Archer.Bow.Lib
 
-let results =
-    [
-        Archer.Tests.Scripts.``Framework Run Should``.``return empty results when it has no tests``
-        Archer.Tests.Scripts.``Framework Run Should``.``return empty results with specific seed when it has no tests``
-        Archer.Tests.Scripts.``Framework Run Should``.``return a successful result when one test passes``
-        Archer.Tests.Scripts.``Framework Run Should``.``return a successful result when two tests pass``
-        Archer.Tests.Scripts.``Framework Run Should``.``return failure when a test fails``
-        Archer.Tests.Scripts.``Framework Run Should``.``return failure when second test fails``
-    ]
-    |> List.map (fun t -> t.LineNumber, t.TestFullName, t.GetExecutor().Execute())
+let framework = archer.Framework ()
+
+[
+    Archer.Tests.Scripts.``Framework Run Should``.``return empty results when it has no tests``
+    Archer.Tests.Scripts.``Framework Run Should``.``return empty results with specific seed when it has no tests``
+    Archer.Tests.Scripts.``Framework Run Should``.``return a successful result when one test passes``
+    Archer.Tests.Scripts.``Framework Run Should``.``return a successful result when two tests pass``
+    Archer.Tests.Scripts.``Framework Run Should``.``return failure when a test fails``
+    Archer.Tests.Scripts.``Framework Run Should``.``return failure when second test fails``
+]
+|> List.iter framework.AddTest
+
+let results = framework.Run ()
     
-printfn "\n"
-results
-|> List.iter (fun (lineNumber, testName, result) ->
-        printfn $"%A{result} <- %s{testName} : %d{lineNumber}"
+printfn $"\nTests Passing: %d{results.Successes |> List.length}, Failing: %d{results.Failures |> List.length}\n"
+
+results.Failures
+|> List.iter (fun (result, test) ->
+        printfn $"%A{result} <- %s{test.TestName} : %d{test.LineNumber}"
     )
 
 printfn "\n\n\n"
 
-exit (results
-      |> List.filter (fun (_, _, result) -> result = TestSuccess |> not)
-      |> List.length)
+exit (results.Failures |> List.length)
