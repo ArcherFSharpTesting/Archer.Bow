aad5223c2baf4cf284cd5bf8e0afc9d6b39e797a
jason-kerney <jason.kerney@edf-re.com>
2023-04-04 10:31:08 -0700

    test: reorganizing tests

--- Tests.Scripts/Program.fs
+++ Tests.Scripts/Program.fs
@@ -6,43 +6,51 @@ open Archer.Tests.Scripts.Scripting
 
 let framework = archer.Framework ()
 
+let testDoublesTests =
+    [
+        ``UnitTest should``.``have the test name``
+        ``UnitTest should``.``have the container name``
+        ``UnitTest should``.``have the container fullname``
+        ``UnitTest should``.``have the test fullname``
+        ``UnitTest should``.``have tags``
+        ``UnitTest should``.``have well formed string representation``
+        
+        ``UnitTestExecutor Happy Path``.``Should have the creating test as its parent``
+        ``UnitTestExecutor Happy Path``.``Should return success if test action returns success``
+        ``UnitTestExecutor Happy Path``.``Should raise ExecutionStart``
+        ``UnitTestExecutor Happy Path``.``Should raise StartSetup``
+        ``UnitTestExecutor Happy Path``.``Should raise EndSetup``
+        ``UnitTestExecutor Happy Path``.``Should raise StartTest``
+        ``UnitTestExecutor Happy Path``.``Should raise EndTest``
+        ``UnitTestExecutor Happy Path``.``Should raise StartTearDown``
+        ``UnitTestExecutor Happy Path``.``Should raise EndExecution``
+        ``UnitTestExecutor Happy Path``.``Should raise all events in correct order``
+        
+        ``UnitTestExecutor Failing Test``.``Should return failure if the test action returns failure``
+        ``UnitTestExecutor Failing Test``.``Should raise all events even if setup fails``
+        ``UnitTestExecutor Failing Test``.``Should return failure if setup fails``
+    ]
+
+let frameWorkTests = 
+    [
+        ``Framework Run Should``.``return empty results when it has no tests``
+        ``Framework Run Should``.``return empty results with specific seed when it has no tests``
+        ``Framework Run Should``.``return a successful result when one test passes``
+        ``Framework Run Should``.``return a successful result when two tests pass``
+        ``Framework Run Should``.``return failure when a test fails``
+        ``Framework Run Should``.``return failure when second test fails``
+        ``Framework Run Should``.``return failure when both tests fail``
+        
+        ``When tests execute normally framework should raise``.``the FrameworkExecutionStarted event``
+        ``When tests execute normally framework should raise``.``the FrameworkExecutionEnded event``
+        // ``When tests execute normally framework should raise``.``the TestExecutionStarted event``
+    ]
+    
 [
-    // Testing The framework built in the tests
-    ``UnitTest should``.``have the test name``
-    ``UnitTest should``.``have the container name``
-    ``UnitTest should``.``have the container fullname``
-    ``UnitTest should``.``have the test fullname``
-    ``UnitTest should``.``have tags``
-    ``UnitTest should``.``have well formed string representation``
-    
-    ``UnitTestExecutor Happy Path``.``Should have the creating test as its parent``
-    ``UnitTestExecutor Happy Path``.``Should return success if test action returns success``
-    ``UnitTestExecutor Happy Path``.``Should raise ExecutionStart``
-    ``UnitTestExecutor Happy Path``.``Should raise StartSetup``
-    ``UnitTestExecutor Happy Path``.``Should raise EndSetup``
-    ``UnitTestExecutor Happy Path``.``Should raise StartTest``
-    ``UnitTestExecutor Happy Path``.``Should raise EndTest``
-    ``UnitTestExecutor Happy Path``.``Should raise StartTearDown``
-    ``UnitTestExecutor Happy Path``.``Should raise EndExecution``
-    ``UnitTestExecutor Happy Path``.``Should raise all events in correct order``
-    
-    ``UnitTestExecutor Failing Test``.``Should return failure if the test action returns failure``
-    ``UnitTestExecutor Failing Test``.``Should raise all events even if setup fails``
-    ``UnitTestExecutor Failing Test``.``Should return failure if setup fails``
-    
-    // Testing Archer Framework
-    ``Framework Run Should``.``return empty results when it has no tests``
-    ``Framework Run Should``.``return empty results with specific seed when it has no tests``
-    ``Framework Run Should``.``return a successful result when one test passes``
-    ``Framework Run Should``.``return a successful result when two tests pass``
-    ``Framework Run Should``.``return failure when a test fails``
-    ``Framework Run Should``.``return failure when second test fails``
-    ``Framework Run Should``.``return failure when both tests fail``
-    
-    ``When tests execute normally framework should raise``.``the FrameworkExecutionStarted event``
-    ``When tests execute normally framework should raise``.``the FrameworkExecutionEnded event``
-    // ``When tests execute normally framework should raise``.``the TestExecutionStarted event``
+    testDoublesTests
+    frameWorkTests
 ]
+|> List.concat
 |> framework.AddTests
 
 let results = framework.Run ()
