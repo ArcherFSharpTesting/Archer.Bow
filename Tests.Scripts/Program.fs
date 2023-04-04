0f089e39cbfb03d0abf9b99a8e95d979e597bb99
jason-kerney <jason.kerney@edf-re.com>
2023-04-04 15:59:24 -0700

    test: moved test to its correct container

--- Tests.Scripts/Program.fs
+++ Tests.Scripts/Program.fs
@@ -49,9 +49,9 @@ let frameWorkTests =
         ``FrameworkExecutionEnded should``.``be raised when the framework is run``
         
         ``TestExecutionStarted should``.``be raised from the given test when framework is run``
-        ``TestExecutionStarted should``.``not be raised if FrameworkExecutionStarted was canceled``
         
         ``TestStartSetup should``.``be raised from the given test when the framework is run``
+        ``TestStartSetup should``.``not be raised if FrameworkExecutionStarted was canceled``
         
         ``TestEndSetup should``.``be raised from the given test when the framework is run``
     ]
--- Tests.Scripts/TestExecutionStartedShould.fs
+++ Tests.Scripts/TestExecutionStartedShould.fs
@@ -1,6 +1,5 @@
 module Archer.Tests.Scripts.``TestExecutionStarted should``
 
-open System.ComponentModel
 open Archer.Bow.Lib
 open Archer.CoreTypes.Lib
 open Archer.Tests.Scripts.TestLang
@@ -38,24 +37,3 @@ let ``be raised from the given test when framework is run`` =
          
          result
      )
-     
-let ``not be raised if FrameworkExecutionStarted was canceled`` =
-    container.Test ("not be raised if FrameworkExecutionStarted was canceled", fun () ->
-        let framework = archer.Framework ()
-        let test = dummyTest None None
-         
-        framework.AddTests [test]
-         
-        framework.FrameworkStartExecution.AddHandler (fun _ (args: CancelEventArgs) ->
-            args.Cancel <- true
-        )
-        
-        let mutable result = TestSuccess
-        framework.TestStartExecution.AddHandler (fun _ _ ->
-            result <- "Event Raised when it was not supposed to be" |> VerificationFailure |> TestFailure
-        )
-        
-        framework.Run () |> ignore
-        
-        result
-    )
