9efd5122cbe738dcb365fffbd66d04e0ae2fd8c5
jason-kerney <jason.kerney@edf-re.com>
2023-04-04 10:09:59 -0700

    test: added dynamic dummy test

--- Tests.Scripts/UnitTestExecutorFailingTestShould.fs
+++ Tests.Scripts/UnitTestExecutorFailingTestShould.fs
@@ -6,13 +6,20 @@ open Archer.Tests.Scripts.TestLang
 open Archer.Tests.Scripts.TestLang.Types
 
 let private container = suite.Container ("Scripting", "UnitTestExecutor Failing Test")
-let private dummyTest () = suite.Container(ignoreString (), ignoreString ()).Test(ignoreString (), successfulTest)
+let private dummyTest (testAction: (unit -> TestResult) option) (parts: TestPart option) =
+    let c = suite.Container (ignoreString (), ignoreString ())
+    match parts, testAction with
+    | None, None -> c.Test (ignoreString (), successfulTest, EmptyPart, ignoreString (), ignoreInt ())
+    | None, Some action -> c.Test (ignoreString (), action, EmptyPart, ignoreString (), ignoreInt ())
+    | Some part, None -> c.Test (ignoreString (), successfulTest, part, ignoreString (), ignoreInt ())
+    | Some part, Some action -> c.Test (ignoreString (), action, part, ignoreString (), ignoreInt ())
+    
 let private notRunError = "Not Run" |> GeneralFailure |> TestFailure
     
 let ``Should return failure if the test action returns failure`` =
     container.Test ("Should return failure if the test action returns failure", fun () ->
         let expectedResult = "Things don't add up" |> VerificationFailure |> TestFailure
-        let test = UnitTest (ignoreString (), ignoreString (), ignoreString (), ignoreInt (), [], (fun () -> expectedResult), EmptyPart) :> ITest
+        let test = dummyTest (Some (fun () -> expectedResult)) None
         
         let result = test.GetExecutor().Execute ()
         
