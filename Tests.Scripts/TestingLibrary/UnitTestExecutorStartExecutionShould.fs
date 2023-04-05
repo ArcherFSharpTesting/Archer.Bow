54b73030d9f579dce2dcce944b49ed9082e9927f
jason-kerney <jason.kerney@edf-re.com>
2023-04-05 11:17:39 -0700

    feat: unittestexecutor does not call testaction

--- Tests.Scripts/TestingLibrary/UnitTestExecutorStartExecutionShould.fs
+++ Tests.Scripts/TestingLibrary/UnitTestExecutorStartExecutionShould.fs
@@ -42,6 +42,24 @@ let ``Test Cases`` = [
         result
     )
     
+    container.Test ("prevent the call of the test action if canceled", fun () ->
+        let mutable result = TestSuccess
+        
+        let testAction () =
+            result <- "Should not be called" |> VerificationFailure |> TestFailure
+            TestSuccess
+            
+        let executor = dummyExecutor (Some testAction) None
+        
+        executor.StartExecution.Add (fun args ->
+            args.Cancel <- true
+        )
+        
+        executor.Execute ()  |> ignore
+        
+        result
+    )
+    
     container.Test ("should cause execution to return a CancelError if canceled", fun () ->
         let executor = dummyExecutor None None
         
