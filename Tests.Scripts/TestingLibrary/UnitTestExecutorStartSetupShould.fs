646500ac6ccf712413f70a3e094b5af9af5cff6f
jason-kerney <jason.kerney@edf-re.com>
2023-04-05 11:30:15 -0700

    test: showing flow of StartSetup

--- Tests.Scripts/TestingLibrary/UnitTestExecutorStartSetupShould.fs
+++ Tests.Scripts/TestingLibrary/UnitTestExecutorStartSetupShould.fs
@@ -1,6 +1,8 @@
 module Archer.Tests.Scripts.TestingLibrary.``UnitTestExecutor StartSetup``
 
 open Archer.Tests.Scripts.TestLang
+open Archer.CoreTypes
+open Archer.Tests.Scripts.TestLang.Types
 
 let private container = suite.Container ("TestingLibrary", "UnitTestExecutor StartSetup should")
 
@@ -17,5 +19,55 @@ let ``Test Cases`` = [
         |> ignore
         
         result
+    )
+    
+    container.Test ("prevent the call of the test setup if canceled", fun () ->
+        let mutable result = TestSuccess
+        
+        let setupPart =
+            SetupPart (fun () ->
+                result <- "Should not be called" |> VerificationFailure |> TestFailure
+                TestSuccess
+            )
+            |> Some
+            
+        let executor = dummyExecutor None setupPart
+        
+        executor.StartSetup.Add (fun args ->
+            args.Cancel <- true
+        )
+        
+        executor.Execute ()  |> ignore
+        
+        result
+    )
+    
+    container.Test ("prevent the call of the test action if canceled", fun () ->
+        let mutable result = TestSuccess
+        
+        let testAction () =
+            result <- "Should not be called" |> VerificationFailure |> TestFailure
+            TestSuccess
+            
+        let executor = dummyExecutor (Some testAction) None
+        
+        executor.StartSetup.Add (fun args ->
+            args.Cancel <- true
+        )
+        
+        executor.Execute ()  |> ignore
+        
+        result
+    )
+    
+    container.Test ("should cause execution to return a CancelError if canceled", fun () ->
+        let executor = dummyExecutor None None
+        
+        executor.StartSetup.Add (fun args ->
+            args.Cancel <- true
+        )
+        
+        executor.Execute ()
+        |> expectsToBe (TestFailure CancelFailure)
     )
 ]
