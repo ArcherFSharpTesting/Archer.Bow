680fab6e4a01b1646b70bf33b2b33306ffe97c84
jason-kerney <jason.kerney@edf-re.com>
2023-04-10 10:31:04 -0700

    test: using microlang to report test failures

--- Tests.Scripts/Program.fs
+++ Tests.Scripts/Program.fs
@@ -2,8 +2,7 @@
 // For more information see https://aka.ms/fsharp-console-apps
 
 open Archer.Bow
-open Archer
-
+open Archer.MicroLang.Lang
 let framework = bow.Framework ()
 
 let frameWorkTests =
@@ -24,70 +23,5 @@ let frameWorkTests =
 frameWorkTests
 |> framework.AddTests
 
-let startTime = System.DateTime.Now
-printfn $"Started at %s{startTime.ToShortTimeString ()}"
-let results = framework.Run ()
-
-let endTime = System.DateTime.Now
-printfn $"Ended at %s{endTime.ToShortTimeString ()}"
-
-let ignored =
-    results.Failures
-    |> List.filter (fun (result, _) ->
-        match result with
-        | IgnoredFailure _
-        | CancelFailure -> true
-        | _ -> false
-    )
-    
-let failures =
-    results.Failures
-    |> List.filter (fun (result, _) ->
-        match result with
-        | IgnoredFailure _
-        | CancelFailure -> false
-        | _ -> true
-    )
-    
-let failureCount = failures |> List.length
-    
-printfn $"\nTests Passing: %d{results.Successes |> List.length}, Ignored: %d{ignored |> List.length} Failing: %d{failureCount}\n"
-
-failures
-|> List.groupBy (fun (_, test) -> test.ContainerPath, test.ContainerName)
-|> List.iter (fun ((containerPath, containerName), (results)) ->
-    printfn $"%s{containerPath}"
-    printfn $"\t%s{containerName}"
-
-    results
-    |> List.iter (fun (failure, test) ->
-        printfn $"\t\t%s{test.TestName}"
-        printfn $"\t\t\t%A{failure}"
-        printfn ""
-        printfn $"\t\t%s{System.IO.Path.Join (test.FilePath, test.FileName)}(%d{test.LineNumber})"
-    )
-)
-
-printfn ""
-
-ignored
-|> List.groupBy (fun (_, test) -> test.ContainerPath, test.ContainerName)
-|> List.iter (fun ((containerPath, containerName), (results)) ->
-    printfn $"%s{containerPath}"
-    printfn $"\t%s{containerName}"
-
-    results
-    |> List.iter (fun (failure, test) ->
-        printfn $"\t\t%s{test.TestName}"
-        printfn $"\t\t\t%A{failure}"
-        printfn ""
-        printfn $"\t\t%s{System.IO.Path.Join (test.FilePath, test.FileName)}(%d{test.LineNumber})"
-    )
-)
-
-printfn $"\n\nTotal Time: %A{endTime - startTime}"
-printfn $"\nSeed: %d{results.Seed}"
-
-printfn "\n"
-
-exit failureCount
+framework
+|> runAndReport
